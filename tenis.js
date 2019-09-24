5fe046ae55546089de6b1715a22098f1f7a7d595
Ferreira Lopes Neto <vrfo@gft.com>
2019-09-24 19:28:55 -0300

    [GFT] - CodingDojo Session 24/09

--- tenis.js
+++ tenis.js
@@ -1,12 +1,14 @@
-const jogadores = [ {
-    score: 5,
-    nome: 'Jose',}
-}, {JOGADOR2: {
-    score: 10,
-    nome: 'Matheus',}
+const jogadores = [{
+    score: 0,
+    nome: 'Jose',
+    vantagem: 0
+}, {
+    score: 0,
+    nome: 'Matheus',
+    vantagem: 0
 }];
 
-let getJogadores = () =>  {
+let getJogadores = () => {
     return jogadores.length;
 }
 
@@ -14,5 +16,33 @@ let getScore = (jogador) => {
     return jogadores[jogador].score;
 }
 
-module.exports.getJogadores = getJogadores();
-module.exports.getScore = getScore();
+let getJogador = (jogador) => {
+    return jogadores[jogador];
+}
+
+let getPontuacao = (jogador) => {
+    let pontuacoes = {0: 15, 15: 30, 30: 40, 40: 40}
+
+    if (pontuacoes[jogador.score] === 40)
+    {
+        //if(jogador.vantagem === 2){
+            return "Vencedor"
+        //}
+    }
+    else{
+        return pontuacoes[jogador.score]
+    }
+} 
+
+let validarEmpate = (jogador1, jogador2) =>{
+    if(jogador1.score === 40 && jogador2.score === 40){
+        return 'Empate'
+    }
+}
+
+
+module.exports.getJogadores = getJogadores;
+module.exports.getScore = getScore;
+module.exports.getPontuacao = getPontuacao;
+module.exports.getJogador = getJogador;
+module.exports.validarEmpate = validarEmpate;
--- tenis.test.js
+++ tenis.test.js
@@ -1,11 +1,42 @@
 const getJogadores = require('./tenis').getJogadores;
 const getScore = require('./tenis').getScore;
+const getPontuacao = require('./tenis').getPontuacao;
+const getJogador = require('./tenis').getJogador;
+const validarEmpate = require('./tenis').validarEmpate;
 
 test('Retorna dois jogadores', () => {  
-    expect(getJogadores).toBe(2);
+    expect(getJogadores()).toBe(2);
 });
 
 test('Retorna pontos', () => {
     let jogador = 1;
     expect(getScore(jogador)).toBeGreaterThanOrEqual(0);
+});
+
+test('Get jogardor by id', () => {
+    let id = 0;
+    expect(getJogador(id).nome).toBe('Jose');
+})
+
+test('Atribuir pontos', () => { 
+    let id = 0;
+    expect(getPontuacao(getJogador(id))).toBe(15);
+});
+
+test('Verifica se venceu', () => { 
+    let id = 0;
+    let jogador = getJogador(id);
+    jogador.score = 40
+    expect(getPontuacao(jogador)).toBe("Vencedor");
+});
+
+test('Verificar empate', () =>{
+    let jogador1 = getJogador(0);
+    let jogador2 = getJogador(1);
+
+    jogador1.score = 40;
+    jogador2.score = 40;
+
+    expect(validarEmpate(jogador1, jogador2)).toBe('Empate');
+
 });
